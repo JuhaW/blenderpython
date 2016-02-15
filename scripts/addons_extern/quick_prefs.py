dc1900ba2b58fd3fad37854e972f9af51bd537be
meta-androcto <meta.androcto1@gmail.com>
2016-02-15 20:50:14 +1100

    update bl info

--- scripts/addons_extern/quick_prefs.py
+++ scripts/addons_extern/quick_prefs.py
@@ -27,7 +27,7 @@ bl_info = {
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/"
                 "Scripts/3D_interaction/QuickPrefs",
     "tracker_url": "https://developer.blender.org/T27822",
-    "category": "Panel"}
+    "category": "3D View"}
 
 
 import os
