c22efa8dc9aa4d7785077f1a5f20c419a9a45e7c
meta-androcto <meta.androcto1@gmail.com>
2016-03-30 19:01:37 +1100

    booltron

---
+++ scripts/addons_extern/blender-addon-booltron-master/__init__.py
@@ -0,0 +1,56 @@
+bl_info = {
+	"name": "Booltron",
+	"author": "Mikhail Rachinskiy (jewelcourses.com)",
+	"version": (2000,),
+	"blender": (2,7,4),
+	"location": "3D View → Tool Shelf (F B)",
+	"description": "Booltron—super add-on for super fast booleans.",
+	"wiki_url": "https://github.com/mrachinskiy/blender-addon-booltron",
+	"tracker_url": "https://github.com/mrachinskiy/blender-addon-booltron/issues",
+	"category": "Object"}
+
+if "bpy" in locals():
+	from importlib import reload
+	reload(helpers)
+	reload(operators)
+	reload(ui)
+	del reload
+else:
+	import bpy
+	from . import operators
+	from . import ui
+
+
+classes = (
+	ui.ToolShelf,
+	ui.Popup,
+
+	operators.UNION,
+	operators.DIFFERENCE,
+	operators.INTERSECT,
+	operators.SEPARATE,
+)
+
+addon_keymaps = []
+
+
+def register():
+	for cls in classes:
+		bpy.utils.register_class(cls)
+
+	kc = bpy.context.window_manager.keyconfigs.addon
+	km = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+	kmi = km.keymap_items.new('wm.call_menu', 'B', 'PRESS', key_modifier='F')
+	kmi.properties.name = 'BOOLTRON_POPUP'
+	addon_keymaps.append((km, kmi))
+
+def unregister():
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
+
+	for km, kmi in addon_keymaps:
+		km.keymap_items.remove(kmi)
+	addon_keymaps.clear()
+
+if __name__ == "__main__":
+	register()
