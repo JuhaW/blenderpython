9abc0edcc26b3ea62d3f90ba7774d2210443d2d2
italic <italic.rendezvous@gmail.com>
2016-06-18 02:04:35 -0700

    Replaced wazou orientation pie with italic pie.

--- scripts/addons_extern/AF_pie_menus/pie_orientation_menu/__init__.py
+++ scripts/addons_extern/AF_pie_menus/pie_orientation_menu/__init__.py
@@ -1,90 +1,82 @@
+"""Replace default list-style menu for transform orientations with a pie."""
 
 bl_info = {
-    "name": "Orientation Menu: Key: 'Alt Space' ",
-    "description": "Orientation Menu",
-    "author": "pitiwazou, meta-androcto",
-    "version": (0, 1, 0),
+    "name": "Orientation Pie",
+    "author": "Italic_",
+    "version": (1, 1, 0),
     "blender": (2, 77, 0),
-    "location": "Alt Space",
-    "warning": "",
-    "wiki_url": "",
-    "category": "3D View"
-}
+    "description": "",
+    "location": "Hotkey: ALT + Spacebar",
+    "category": "Pie Menu"}
+
 
 import bpy
-from ..utils import AddonPreferences, SpaceProperty
-from bpy.types import Menu, Header
-from bpy.props import IntProperty, FloatProperty, BoolProperty, StringProperty
+from bpy.types import Menu, Operator
 
-class OrientationVariable(bpy.types.Operator):
-    bl_idname = "object.orientationvariable"
-    bl_label = "Orientation Variable"
-    bl_options = {'REGISTER', 'UNDO'}
-    variable = bpy.props.StringProperty()
+
+class OrientPoll(Operator):
+    bl_idname = "pie.orientation"
+    bl_label = "Orientation Poll"
+    bl_options = {'INTERNAL'}
+    space = bpy.props.StringProperty()
 
     @classmethod
     def poll(cls, context):
-        return True
+        return bpy.context.space_data.type == "VIEW_3D"
 
     def execute(self, context):
-        bpy.context.space_data.transform_orientation = self.variable
+        bpy.context.space_data.transform_orientation = self.space
         return {'FINISHED'}
 
-# Pie Orientation - Alt + Space
-class PieOrientation(Menu):
-    bl_idname = "pie.orientation"
-    bl_label = "Pie Orientation"
+
+class OrientPie(Menu):
+    bl_label = "Transform Orientation"
 
     def draw(self, context):
         layout = self.layout
-
         pie = layout.menu_pie()
-        # 4 - LEFT
-        pie.operator("object.orientationvariable", text="View").variable = 'VIEW'
-        # 6 - RIGHT
-        pie.operator("object.orientationvariable", text="Local").variable = 'LOCAL'
-        # 2 - BOTTOM
-        pie.operator("object.orientationvariable", text="Normal").variable = 'NORMAL'
-        # 8 - TOP
-        pie.operator("object.orientationvariable", text="Global").variable = 'GLOBAL'
-        # 7 - TOP - LEFT
-        pie.operator("object.orientationvariable", text="Gimbal").variable = 'GIMBAL'
-        # 9 - TOP - RIGHT
-        # 1 - BOTTOM - LEFT
-        # 3 - BOTTOM - RIGHT
+        view = context.space_data
 
-classes = [
-    OrientationVariable,
-    PieOrientation,
-    ]
+        pie.operator("pie.orientation", text="Global").space = 'GLOBAL'
+        pie.operator("pie.orientation", text="Local").space = 'LOCAL'
+        pie.operator("pie.orientation", text="Gimbal").space = 'GIMBAL'
+
+        # XXX: Display only custom orientations
+        pie = pie.box()
+        pie.prop(view, "transform_orientation", text="")
+        pie = layout.menu_pie()
+
+        pie.operator("pie.orientation", text="Normal").space = 'NORMAL'
+        pie.operator("pie.orientation", text="View").space = 'VIEW'
+
 
 addon_keymaps = []
 
+
 def register():
-    for cls in classes:
-        bpy.utils.register_class(cls)
+    bpy.utils.register_module(__name__)
+
     wm = bpy.context.window_manager
 
-    if wm.keyconfigs.addon:
-        # Orientation
-        km = wm.keyconfigs.addon.keymaps.new(name='3D View Generic', space_type='VIEW_3D')
-        kmi = km.keymap_items.new('wm.call_menu_pie', 'SPACE', 'PRESS', alt=True)
-        kmi.properties.name = "pie.orientation"
-        kmi.active = True
-        addon_keymaps.append((km, kmi))
+    km = wm.keyconfigs.addon.keymaps['3D View']
+    kmi = km.keymap_items.new('wm.call_menu_pie', 'SPACE', 'PRESS', alt=True)
+    kmi.properties.name = "OrientPie"
+    addon_keymaps.append(km)
+
 
 def unregister():
-    for cls in classes:
-        bpy.utils.unregister_class(cls)
-    wm = bpy.context.window_manager
+    bpy.utils.unregister_module(__name__)
 
-    kc = wm.keyconfigs.addon
-    if kc:
-        km = kc.keymaps['3D View Generic']
-        for kmi in km.keymap_items:
-            if kmi.idname == 'wm.call_menu_pie':
-                if kmi.properties.name == "pie.orientation":
-                    km.keymap_items.remove(kmi)
+    wm = bpy.context.window_manager
+    if wm.keyconfigs.addon:
+        for km in addon_keymaps:
+            for kmi in km.keymap_items:
+                km.keymap_items.remove(kmi)
+
+            # wm.keyconfigs.addon.keymaps.remove(km)
+
+    addon_keymaps.clear()
+
 
 if __name__ == "__main__":
     register()
