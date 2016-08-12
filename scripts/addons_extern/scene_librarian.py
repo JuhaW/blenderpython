4df0d187bcadd0c0693b2ab3bd9947fa289c7e9d
italic <italic.rendezvous@gmail.com>
2016-08-11 21:52:31 -0700

    Update librarian.

--- scripts/addons_extern/scene_librarian.py
+++ scripts/addons_extern/scene_librarian.py
@@ -20,8 +20,8 @@ bl_info = {
     "name": "Librarian",
     "description": "View what datablocks are coming from which linked libraries",
     "author": "Greg Zaal",
-    "version": (0, 2),
-    "blender": (2, 76, 0),
+    "version": (0, 3),
+    "blender": (2, 77, 0),
     "location": "Properties Editor > Scene > Librarian panel",
     "warning": "",
     "wiki_url": "",
@@ -30,6 +30,8 @@ bl_info = {
 
 
 import bpy
+import os
+from bpy_extras.io_utils import ImportHelper
 
 '''
 TODO:
@@ -49,11 +51,10 @@ def get_linked_data():
 
     for attr in dir(bpy.data):
         data_iter = getattr(bpy.data, attr, None)
-        if isinstance(data_iter, type_iter):
+        if type(data_iter) == type_iter:
             for id_data in data_iter:
                 if id_data.library:
                     yield id_data
-
 
 def count_type(data, rna_type):
     c = 0
@@ -61,7 +62,6 @@ def count_type(data, rna_type):
         if d.bl_rna.name == rna_type:
             c += 1
     return c
-
 
 def count_types(data):
     rna_types = {}
@@ -81,7 +81,6 @@ def count_types(data):
             rna_types[t] = 1
     return rna_types
 
-
 def type_icon(t):
 
     # Special cases:
@@ -93,35 +92,34 @@ def type_icon(t):
         return "TEXTURE"  # Image/Cloud etc have different types, but all include "Texture"
 
     d = {
-        'Action': 'ACTION',
-        'Armature': 'ARMATURE_DATA',
-        'Brush': 'BRUSH_DATA',
-        'Camera': 'CAMERA_DATA',
-        'Curve': 'CURVE_DATA',
-        'Grease Pencil': 'GREASEPENCIL',
-        'Group': 'GROUP',
-        'Image': 'IMAGE_DATA',
-        'Lattice': 'LATTICE_DATA',
-        'Mask': 'MOD_MASK',
-        'Material': 'MATERIAL',
-        'Mesh': 'MESH_DATA',
-        'MetaBall': 'META_DATA',
-        'MovieClip': 'CLIP',
-        'Object': 'OBJECT_DATAMODE',
-        'Particle Settings': 'PARTICLES',
-        'Scene': 'SCENE_DATA',
-        'Speaker': 'SPEAKER',
-        'Surface Curve': 'SURFACE_DATA',
-        'Text': 'TEXT',
-        'Text Curve': 'FONT_DATA',
-        'Vector Font': 'FONT_DATA',
-        'World': 'WORLD'
+    'Action': 'ACTION',
+    'Armature': 'ARMATURE_DATA',
+    'Brush': 'BRUSH_DATA',
+    'Camera': 'CAMERA_DATA',
+    'Curve': 'CURVE_DATA',
+    'Grease Pencil': 'GREASEPENCIL',
+    'Group': 'GROUP',
+    'Image': 'IMAGE_DATA',
+    'Lattice': 'LATTICE_DATA',
+    'Mask': 'MOD_MASK',
+    'Material': 'MATERIAL',
+    'Mesh': 'MESH_DATA',
+    'MetaBall': 'META_DATA',
+    'MovieClip': 'CLIP',
+    'Object': 'OBJECT_DATAMODE',
+    'Particle Settings': 'PARTICLES',
+    'Scene': 'SCENE_DATA',
+    'Speaker': 'SPEAKER',
+    'Surface Curve': 'SURFACE_DATA',
+    'Text': 'TEXT',
+    'Text Curve': 'FONT_DATA',
+    'Vector Font': 'FONT_DATA',
+    'World': 'WORLD'
     }
     if t in d:
         return d[t]
     else:
         return "QUESTION"  # default icon
-
 
 def pad_lib_name(lib):
     """ Used to ensure lib names do not match inside each other ('Lib' won't match 'Lib.001') """
@@ -142,6 +140,60 @@ class LibrarianToggleExpand(bpy.types.Operator):
             expanded += self.lib
 
         context.scene.librarian_settings.expanded = expanded
+
+        return {'FINISHED'}
+
+
+class LibrarianImportBlend(bpy.types.Operator, ImportHelper):
+    """Import all objects from another file into the current scene, keeping the linked libraries in tact"""
+    bl_idname = 'librarian.import'
+    bl_label = 'Import Objects from File'
+    bl_options = {'REGISTER', 'UNDO'}
+    directory = bpy.props.StringProperty(subtype="DIR_PATH")
+    filename = bpy.props.StringProperty(subtype="FILE_NAME")    
+    # files = CollectionProperty(type=bpy.types.OperatorFileListElement, options={'HIDDEN', 'SKIP_SAVE'})
+
+    def execute(self, context):
+        directory = self.directory
+        filename = self.filename
+
+        # DEBUG
+        print ("\nDIR:", directory)
+        print ("FN:", filename)
+
+        if not filename:
+            self.report({'ERROR'}, "No file chosen")
+            return {'CANCELLED'}
+
+        filepath = os.path.join(directory, filename)
+
+        if not os.path.exists(filepath):
+            self.report({'ERROR'}, filepath+" does not exist!")
+            return {'CANCELLED'}
+
+        # Get all the scenes from the selected file
+        with bpy.data.libraries.load(filepath) as (data_from, data_to):
+            data_to.scenes = data_from.scenes
+        
+        # Add all objects from those scenes into the current scene
+        cur_scene = context.scene
+        scene_names = []
+        for sc in data_to.scenes:
+            scene_names.append (sc.name)
+            for obj in sc.objects:
+                cur_scene.objects.link(obj)
+                obj.select = True
+
+        print ("2")
+        context.screen.scene = cur_scene
+        print ("3")
+
+        # Remove imported scenes
+        print ("4")
+        for sc in data_to.scenes:
+            print ("5:", sc)
+            bpy.data.scenes.remove(sc)
+            print ("6:", sc)
 
         return {'FINISHED'}
 
@@ -188,13 +240,15 @@ class LibrarianImagePathsPanel(bpy.types.Panel):
                     col.label(d.name, icon=type_icon(d.rna_type.name))
         if len(libs) == 0:
             maincol.label("There are no linked libraries :)")
+        
+        maincol.operator('librarian.import', icon='LIBRARY_DATA_DIRECT')
+        
 
 
 def register():
     bpy.utils.register_module(__name__)
 
     bpy.types.Scene.librarian_settings = bpy.props.PointerProperty(type=LibrarianSettings)
-
 
 def unregister():
     del bpy.types.Scene.librarian_settings
