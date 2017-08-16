c399d5dd67c6def820899a03458233c5cb218294
Italic- <super3dblender@yahoo.com>
2017-08-15 17:10:21 -0700

    Animator's Toolbox: cleanup: whitespace, style, imports.
    (cherry picked from commit 0c95bed42d82622782e2901b99250688036785c8)

--- scripts/addons_extern/Animators_Toolbox.py
+++ scripts/addons_extern/Animators_Toolbox.py
@@ -12,30 +12,30 @@ bl_info = {
     "name": "Animator's Toolbox",
     "description": "A set of tools specifically for animators.",
     "author": "Brandon Ayers (thedaemon)",
-    "version": (0, 5, 1),
+    "version": (0, 5, 2),
     "blender": (2, 78, 0),
     "location": "View3D > Toolbar > Animation > Animator's Toolbox",
-    "warning": "", # used for warning icon and text in addons panel
+    "warning": "",  # used for warning icon and text in addons panel
     "wiki_url": "https://github.com/thedaemon/Blender-Scripts",
     "tracker_url": "https://github.com/thedaemon/Blender-Scripts/issues",
     "category": "Animation"
-    }
+}
 
-import os.path
 import bpy
-from bpy.props import *
-from bpy.types import Operator
+from bpy.props import BoolProperty, IntProperty
+from bpy.types import Operator, PropertyGroup, Panel
 
 KEYMAPS = list()
 
 
 # FEATURE: Jump forward/backward every N frames. Currently hardcoded variable.
 # the frame jump amount needs to be in preferences along with hotkey selector (viewer?)
-class AnimatorsToolboxFrameJump(bpy.types.Operator):
+class AnimatorsToolboxFrameJump(Operator):
     """Jump a number of frames forward/backwards"""
     bl_idname = "screen.animatorstools_frame_jump"
     bl_label = "Jump Frames"
     forward = bpy.props.BoolProperty(default=True)
+
     def execute(self, context):
         scene = context.scene
         framedelta = 4
@@ -48,11 +48,14 @@ class AnimatorsToolboxFrameJump(bpy.types.Operator):
 
 # FEATURE: A toggle to keep the animator from selecting something other
 # than the Armature.
-class ToggleSelectability(bpy.types.Operator):
-    """Turns off selection for all objects
-    leaving only Armatures selectable"""
+class ToggleSelectability(Operator):
+    """
+    Turns off selection for all objects
+    leaving only Armatures selectable
+    """
     bl_idname = "bone.toggleselectability"
     bl_label = "Armature Selection Only"
+
     def execute(self, context):
         do_i_hide_select = not bpy.context.active_object.hide_select
         if bpy.context.selected_objects == []:
@@ -80,11 +83,12 @@ class ToggleSelectability(bpy.types.Operator):
 
 # Useless because blender already has this freaking command but I programmed
 # it anyways. I may use it for specific axis template.
-class ClearAllTransforms(bpy.types.Operator):
+class ClearAllTransforms(Operator):
     """Clears all transforms on the bone I hope"""
     bl_idname = "pose.clearall"
     bl_label = "Clear Transforms"
-    def execute(self,context):
+
+    def execute(self, context):
         for object in bpy.data.objects:
             if object.type == 'ARMATURE':
                 bpy.ops.pose.rot_clear()
@@ -95,53 +99,57 @@ class ClearAllTransforms(bpy.types.Operator):
 
 # FEATURE: A toggle for OpenSubdiv on all objects in scene with a Subdivision
 # Surface Modifier.
-class ToggleOpensubdiv(bpy.types.Operator):
-    """Toggles OpenSubdiv for all Objects for
-    improved animation playback speed"""
+class ToggleOpensubdiv(Operator):
+    """
+    Toggles OpenSubdiv for all Objects for
+    improved animation playback speed
+    """
     bl_idname = "mesh.opensubdiv"
     bl_label = "Mesh OpenSubdiv"
-    # Does nothing, testing.
-    my_bool = bpy.props.BoolProperty(name="Toggle Option")
-    def execute(self,context):
+
+    def execute(self, context):
         for mm in (m for o in bpy.context.scene.objects
-                   for m in o.modifiers if m.type=='SUBSURF'):
-            if mm.use_opensubdiv == True:
+                   for m in o.modifiers if m.type == 'SUBSURF'):
+            if mm.use_opensubdiv is True:
                 mm.use_opensubdiv = False
             else:
-                if mm.use_opensubdiv == False:
+                if mm.use_opensubdiv is False:
                     mm.use_opensubdiv = True
         return{'FINISHED'}
 
 
 # Feature: Turns OpenSubdiv on for all meshes with Subdivision Surface
 # Modifiers for improved viewport performance.
-class OpensubdivOn(bpy.types.Operator):
+class OpensubdivOn(Operator):
     bl_idname = "opensubdiv.on"
     bl_label = "OpenSubdiv On"
-    def execute(self,context):
+
+    def execute(self, context):
         for mm in (m for o in bpy.context.scene.objects
-                   for m in o.modifiers if m.type=='SUBSURF'):
+                   for m in o.modifiers if m.type == 'SUBSURF'):
             mm.use_opensubdiv = True
         return{'FINISHED'}
 
 
 # Feature: Turns OpenSubdiv on for all meshes with Subdivision Surface
 # Modifiers for improved viewport performance.
-class OpensubdivOff(bpy.types.Operator):
+class OpensubdivOff(Operator):
     bl_idname = "opensubdiv.off"
     bl_label = "OpenSubdiv Off"
-    def execute(self,context):
+
+    def execute(self, context):
         for mm in (m for o in bpy.context.scene.objects
-                   for m in o.modifiers if m.type=='SUBSURF'):
+                   for m in o.modifiers if m.type == 'SUBSURF'):
             mm.use_opensubdiv = False
         return{'FINISHED'}
 
 
 # FEATURE: Simple X-Ray toggle for Armature
-class ToggleXray(bpy.types.Operator):
+class ToggleXray(Operator):
     """Toggles X-Ray mode for bones"""
     bl_idname = "bone.togglexray"
     bl_label = "Armature X-Ray"
+
     def execute(self, context):
         for object in bpy.data.objects:
             if object.type == 'ARMATURE':
@@ -151,10 +159,11 @@ class ToggleXray(bpy.types.Operator):
 
 # FEATURE: Simple Auto-IK toggle for Armature
 # context.active_object.data, "use_auto_ik", text="Auto IK")
-class ToggleAutoIK(bpy.types.Operator):
+class ToggleAutoIK(Operator):
     """Toggles Auto IK mode for bones"""
     bl_idname = "bone.toggleautoik"
     bl_label = "Armature Auto IK"
+
     def execute(self, context):
         for object in bpy.data.active_object:
             if object.type == 'ARMATURE':
@@ -162,11 +171,12 @@ class ToggleAutoIK(bpy.types.Operator):
         return{'FINISHED'}
 
 
-class QuickMotionPath(bpy.types.Operator):
+class QuickMotionPath(Operator):
     """Create motion path by preview range"""
     bl_label = "Calculate"
     bl_idname = "cenda.motion_pats"
-    def execute(self, context ):
+
+    def execute(self, context):
         if(bpy.context.scene.use_preview_range):
             startFrame = bpy.context.scene.frame_preview_start
             endFrame = bpy.context.scene.frame_preview_end
@@ -177,33 +187,32 @@ class QuickMotionPath(bpy.types.Operator):
         return{'FINISHED'}
 
 
-## UI --
-from bpy.types import PropertyGroup, Panel
-from bpy.props import *
-
+# UI --
 
 class animatorstoolboxData(PropertyGroup):
-    bl_idname = 'animatorstoolboxDataUI'
     """
     UI property group for the add-on  WORK IN PROGRESS
     Options to adjust how the panel is displayed.
     bpy > types > WindowManager > animatorstoolboxDataUI
     bpy > context > window_manager > animatorstoolboxDataUI
     """
-    displayMode = BoolProperty(name='Display Mode', description="Use"
-                               "this to hide many of the options below that"
-                               "are generally needed while rigging."
-                               "(Useful for animating.)",
-                               default=False)
-    deformOptions = BoolProperty(name='Deform Options', description="Disp"
-                                    "lay the deform options for this bone.",
-                                 default=False)
+    bl_idname = 'animatorstoolboxDataUI'
+    displayMode = BoolProperty(
+        name='Display Mode', description="Use"
+        "this to hide many of the options below that"
+        "are generally needed while rigging."
+        "(Useful for animating.)",
+        default=False)
+    deformOptions = BoolProperty(
+        name='Deform Options',
+        description="Display the deform options for this bone.",
+        default=False)
     breakdowner_percentage = IntProperty(
-        name = 'Breakdowner Percentage',
-        description = ' a percentage for the Breakdowner Button.',
-        default = 50,
-        min = 0,
-        max = 100)
+        name='Breakdowner Percentage',
+        description=' a percentage for the Breakdowner Button.',
+        default=50,
+        min=0,
+        max=100)
 
 
 def draw_animatorstoolbox_panel(context, layout):
@@ -222,11 +231,13 @@ def draw_animatorstoolbox_panel(context, layout):
     box.operator("pose.transforms_clear", text="Reset All")
     box.operator("show_x_ray", text="X-Ray")
     box.operator("bone.toggleselectability", text="Select Armature Only")
+
     col = layout.column(align=True)
     row = col.row(align=True)
     row.operator("pose.copy", text="Copy")
     row.operator("pose.paste", text="Paste").flipped = False
     row.operator("pose.paste", text="Flip").flipped = True
+
     col = layout.column(align=True)
     row = col.row(align=True)
     row.prop(toolsettings, "use_keyframe_insert_auto", text="", toggle=True)
@@ -237,12 +248,14 @@ def draw_animatorstoolbox_panel(context, layout):
             subsub = row.row()
             subsub.prop(toolsettings, "use_record_with_nla", toggle=True)
     row.prop_search(scene.keying_sets_all, "active", scene, "keying_sets_all", text="")
+
     col = layout.column(align=True)
     row = col.row(align=True)
     row.operator("pose.relax", text="Relax")
     row.operator("pose.breakdown", text="Tween")
     row.operator("pose.push", text="Push")
-    #row.prop(scene, 'breakdowner_percentage', slider=True )
+    # row.prop(scene, 'breakdowner_percentage', slider=True )
+
     #--Motion Path
     pchan = context.active_pose_bone
     mpath = pchan.motion_path if pchan else None
@@ -253,22 +266,24 @@ def draw_animatorstoolbox_panel(context, layout):
         row.operator("pose.paths_update", text="Update")
         row.operator("pose.paths_clear", text="", icon='X')
     else:
-        #col.operator("pose.paths_calculate", text="Calculate")
-        col.operator("cenda.motion_pats", icon = 'ANIM_DATA')
+        # col.operator("pose.paths_calculate", text="Calculate")
+        col.operator("cenda.motion_pats", icon='ANIM_DATA')
     col = layout.column(align=True)
     row = col.row(align=True)
     row.operator("opensubdiv.on", text="OpenSubdiv")
     row.operator("opensubdiv.off", text="Off")
     col.separator()
-#--Simplify
-    #col = layout.column(align=True)
-    #col.label(text="Simplify:")
+
+    #--Simplify
+    # col = layout.column(align=True)
+    # col.label(text="Simplify:")
     col = layout.column(align=True)
     row = col.row(align=True)
     row.prop(render, "use_simplify", text="Simplify")
     row = col.row(align=True)
     row.prop(render, "simplify_subdivision", text="Subdivision")
-#--New Key Type
+
+    #--New Key Type
     col.separator()
     col = layout.column(align=True)
     row = col.row()
@@ -279,21 +294,23 @@ def draw_animatorstoolbox_panel(context, layout):
     row.prop(toolsettings, "keyframe_type", text="", icon_only=False)
 
 
-class AnimatorsToolBox(bpy.types.Panel):
+class AnimatorsToolBox(Panel):
     """Creates a custom Animator Panel in the 3D View"""
     bl_label = "Animator's Toolbox"
     bl_idname = "ANIM_TOOLBOX"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS'
     bl_category = "Animation"
-#--Header
+
+    #--Header
     def draw_header(self, context):
         layout = self.layout
-#animatorstoolboxDataUIProps = context.window_manager.animatorstoolboxDataUI
-        #layout.prop('animatorstoolboxDataUI', "displayMode", text="")
+        # animatorstoolboxDataUIProps = context.window_manager.animatorstoolboxDataUI
+        # layout.prop('animatorstoolboxDataUI', "displayMode", text="")
         DoButton = self.layout
-        DoButton.operator('bs.doboom', text = '', icon = 'RENDER_ANIMATION')
-#--Draw Toolboxes
+        DoButton.operator('bs.doboom', text='', icon='RENDER_ANIMATION')
+
+    #--Draw Toolboxes
     def draw(self, context):
         layout = self.layout
         draw_animatorstoolbox_panel(context, layout)
@@ -312,6 +329,7 @@ classes = (
     animatorstoolboxData,
     AnimatorsToolBox
 )
+
 
 def register():
     for cls in classes:
@@ -342,5 +360,7 @@ def unregister():
     for km, kmi in KEYMAPS:
         km.keymap_items.remove(kmi)
     KEYMAPS.clear()
+
+
 if __name__ == "__main__":
     register()
